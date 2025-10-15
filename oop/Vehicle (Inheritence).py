3e07c627e9123c97c179c91ff6c9196f7293a133
Sidh10 <94279309+Sidh10@users.noreply.github.com>
2025-10-15 18:31:27 +0530

    Create Vehicle (Inheritence).py
    This question shows the use of inheritence by making parent class and subclass , inherenting methods and args from the parent class and also overiding them.

---
+++ oop/Vehicle (Inheritence).py
@@ -0,0 +1,39 @@
+class Vehicle:
+    def __init__(self,brand,model,year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+    def start_engine():
+        print("Engine Started")
+    def display_info(self):
+        print(f"Car brand: {self.brand}")    
+        print(f"Car model : {self.model}")
+        print(f"Year of manufacturing : {self.year}")   
+class Car(Vehicle):
+    def __init__(self,brand,model,year,seat,fuel_type):
+        super().__init__(brand,model,year)
+        self.seat = seat
+        self.fuel_type = fuel_type
+    
+    
+    def display_info(self) :
+        print(f"Car brand: {self.brand}")    
+        print(f"Car model : {self.model}")
+        print(f"Year of manufacturing : {self.year}")
+        print(f"Seat type :{self.seat}")
+        print(f"Fuel Type : {self.fuel_type}")
+    def drive(self):
+        print("Car is now driving")
+
+class ElectricCar(Car):
+    def __init__(self,brand,model,year,seat,fuel_type,battery_capacity):
+        super().__init__(brand,model,year,seat,fuel_type)
+        self.battery_capacity = battery_capacity
+    def display_info(self):
+         super().display_info()
+         print(f"battery_capacity :{self.battery_capacity}")  
+    def charge(self):
+        print("Charging Battery")       
+
+c1 = ElectricCar('Tesla','Model S',2024,5,'Electric','100 kWh')
+c1.display_info()
